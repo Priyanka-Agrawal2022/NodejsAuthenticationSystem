487a395f9417a698b0e91488ad6ddb74fdeed4ef
Priyanka-Agrawal2022 <prnkgrwl286@gmail.com>
2023-07-18 23:45:11 +0530

    add mongodb atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const env = require("../config/environment");
 
-mongoose.connect(`mongodb://0.0.0.0/${env.db}`);
+// mongoose.connect(`mongodb://0.0.0.0/${env.db}`);
 
-// mongoose.connect(`${env.mongodb_atlas_uri}`);
+mongoose.connect(`${env.mongodb_atlas_uri}`);
 
 const db = mongoose.connection;
 
